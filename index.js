bd9628568fb1ed062f92b0cc9d82bbc57abbc2e8
Elle Oshiro <ekoshiro16@gmail.com>
2023-03-22 23:06:28 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/*
+You are the owner of a fairly successful convention hall. You've hosted all sorts of conventions, but in all your time in running your hall, you've kept track of all your expenses on paper. You've been looking to modernize your accounting system, so you have been learning some programming skills on the side. You want to figure out some way to figure out which booths at a given convention are the most successful. 
+
+Write an algorithm that accepts a string input, where each character in the string input represents a sale made at a given booth. Sales made at the same booth are represented by the same character. 
+
+Your algorithm should return the character in the string that has made the most sales. 
+
+For example: 
+If you were given a string like "aabbccc"
+Booth a made 2 sales, booth b made 2 sales, and booth c made 3 sales. 
+So your algorithm would retun "c".
+
+If you were given a string like "abcdee"
+Your algorithm would return "e", since booth E has made the most sales in the given string.
+
+If you are given a string where no character appears more than any other, return the first character of the string. 
+
+Note: You may not use the Math.max built in method. 
+*/
+
+// Write your code below: 
